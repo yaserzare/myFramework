4d8c514263d19ca2c0bdf5c69016ce84737d52cf
yaserzare <info@yaserzare.ir>
2023-12-30 13:18:04 +0330

    Initialize myFramework

---
+++ app/Application.php
@@ -0,0 +1,12 @@
+<?php
+namespace App;
+
+class Application
+{
+
+    public function test()
+    {
+        echo "Hi World";
+    }
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__."/vendor/autoload.php";
+
+$app = new \App\Application();
+
+var_dump($app->test());
+
+
